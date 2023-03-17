83f5ffc2ff31fc041613166bfd635f511d925e9f
danieljuma5 <jumadaniel275@gmail.com>
2023-03-18 01:40:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// findMatching
+function findMatching(driver,str) {
+  return driver.filter(driver => driver.toUpperCase() === str.toUpperCase()) ;
+}
+
+
+
+//fuzzyMatch
+
+function fuzzyMatch(driver,str) {
+  return driver.filter(driver => driver.toLowerCase().startsWith(str.toLowerCase()));
+}
+
+
+//matchName
+function matchName(driver, str) {
+  return driver.filter(driver => driver.name.toUpperCase() === str.toUpperCase())
+}
